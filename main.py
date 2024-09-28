0ebfe217a35b020f4fd38475c6106af45325c755
Srijan kumar pandey <121687654+srijanp56@users.noreply.github.com>
2024-09-28 23:28:50 +0530

    Band Name Generator
    The Band Name Generator is a fun and interactive Python script designed to create unique band names based on user input. By simply providing the name of the city you grew up in and your pet's name, the program generates a catchy and personalized band name that you can imagine performing on stage!

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+city=input("What's the name of city you grew up in?\n")
+pet=input("What's your pet's name?\n")
+print("Your Band name could be", city+" "+pet)
+
+
